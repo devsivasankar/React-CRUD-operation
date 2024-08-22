213c6c2563869e4aaaf798e7d1a728ae9fc929e0
devsivasankar <mypostbox.sivasankar@gmail.com>
2024-08-22 16:40:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Userdata from './components/userdata';
+
+function App() {
+  return (
+    <div className="App">
+      <Userdata></Userdata>
+    </div>
+  );
+}
+
+export default App;
